71548c24ca41196ebc0d2364fc1a778dd74e3435
Javier Garcia <javier@rednodelabs.com>
2022-06-05 14:25:17 +0200

    Start openthread only managed by rednodebus

--- samples/coap_client/src/coap_client_utils.c
+++ samples/coap_client/src/coap_client_utils.c
@@ -282,7 +282,7 @@ void coap_client_utils_init(ot_connection_cb_t on_connect,
 	k_work_init(&provisioning_work, send_provisioning_request);
 
 	openthread_set_state_changed_cb(on_thread_state_changed);
-	openthread_start(openthread_get_default_context());
+	// openthread_start(openthread_get_default_context()); // Managed by RedNodeBus
 
 	if (IS_ENABLED(CONFIG_OPENTHREAD_MTD_SED)) {
 		k_work_init(&toggle_MTD_SED_work,
